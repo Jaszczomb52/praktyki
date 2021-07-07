f82d92ae4fa77af5327d5e8ddc52d55d74a1eb94
jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-07 12:27:26 +0200

    gra 1

---
+++ gra1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace gra1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            canv.CaptureMouse();
+            ContentControl content = new ContentControl();
+            content.Template = Resources["templ"] as ControlTemplate;
+            Anim(content, 0, canv.ActualWidth - 100, "(Canvas.Left)");
+        }
+
+        private void Anim(ContentControl cont, double p1, double p2, string p3)
+        {
+            Storyboard story = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = p1,
+                To = p2,
+                Duration = new Duration(TimeSpan.FromSeconds(10))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(p3));
+            story.Children.Add(animation);
+            story.Begin();
+        }
+    }
+}
