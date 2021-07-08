56d0eec1f1ea4087194e210bd250556a74cd460d
jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-08 09:01:15 +0200

    dodawanie kółek za pomocą przycisku

--- gra1/MainWindow.xaml.cs
+++ gra1/MainWindow.xaml.cs
@@ -24,25 +24,52 @@ namespace gra1
         public MainWindow()
         {
             InitializeComponent();
-            ContentControl content = new ContentControl();
-            content.Template = Resources["templ"] as ControlTemplate;
-            Anim(content, canv.Width-70, canv.ActualWidth, "(Canvas.Left)");
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.StartPoint = new Point(0, 0);
+            brush.EndPoint = new Point(1, 1);
+            brush.GradientStops.Add(new GradientStop(Colors.Blue, 0.0));
+            brush.GradientStops.Add(new GradientStop(Colors.Red, 1.0));
+            canv.Background = brush;
         }
-
-        private void Anim(ContentControl cont, double p1, double p2, string p3)
+        Random rand = new Random();
+        private void AddEnemy()
         {
-            Random rand = new Random();
+            ContentControl enemy = new ContentControl();
+            Ellipse el = new Ellipse();
+            el.Width = 100;
+            el.Height = 100;
+            Canvas.SetLeft(el, 500);
+            
+            ImageBrush img = new ImageBrush();
+            img.ImageSource = new BitmapImage(new Uri("C:/Users/xopero/Downloads/OIP.jpg"));
+            
+            el.Fill = img;
+            enemy.Content = el;
+            AnimateEnemy(enemy, canv.ActualWidth - 100, 0, "(Canvas.Left)");
+            AnimateEnemy(enemy, rand.Next((int)canv.ActualHeight-100),
+                rand.Next((int)canv.ActualHeight-100), "(Canvas.Top)");
+            canv.Children.Add(enemy);
+            
+        }
+        private void AnimateEnemy(ContentControl cont, double p1, double p2, string p3)
+        {
+
             Storyboard story = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
             DoubleAnimation animation = new DoubleAnimation()
             {
                 From = p1,
                 To = p2,
-                Duration = new Duration(TimeSpan.FromSeconds(rand.Next(2,6)))
+                Duration = new Duration(TimeSpan.FromSeconds(rand.Next(2,3)))
             };
-            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTarget(animation, cont);
             Storyboard.SetTargetProperty(animation, new PropertyPath(p3));
             story.Children.Add(animation);
             story.Begin();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddEnemy();
         }
     }
 }
