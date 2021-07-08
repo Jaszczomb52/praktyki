fb798652b698a6dec95fea19668b2a302209891b
jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-08 09:45:58 +0200

    dodanie etapow itp.

--- gra1/MainWindow.xaml.cs
+++ gra1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace gra1
 {
@@ -24,14 +25,53 @@ namespace gra1
         public MainWindow()
         {
             InitializeComponent();
+            // gradient brush tla
             LinearGradientBrush brush = new LinearGradientBrush();
             brush.StartPoint = new Point(0, 0);
             brush.EndPoint = new Point(1, 1);
             brush.GradientStops.Add(new GradientStop(Colors.Blue, 0.0));
             brush.GradientStops.Add(new GradientStop(Colors.Red, 1.0));
             canv.Background = brush;
+            // interval przeciwnika
+            enemyTimer.Tick += enemyTimer_Tick;
+            enemyTimer.Interval = TimeSpan.FromSeconds(2);
+            // interval targetu
+            targetTimer.Tick += targetTimer_Tick;
+            targetTimer.Interval = TimeSpan.FromSeconds(0.1);
+            canv.Children.Clear();
         }
+
+        //reakcja przeciwnika co tick
+        private void enemyTimer_Tick(object sender, EventArgs e)
+        {
+            AddEnemy();
+        }
+
+        //reakcja targetu co tick
+        private void targetTimer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 1;
+            if (progressBar.Value == progressBar.Maximum)
+                end();
+        }
+
+        //reakcja na koniec gry
+        private void end()
+        {
+            canv.Children.Add(endText);
+            enemyTimer.Stop();
+            targetTimer.Stop();
+            humanCaptured = false;
+            startButt.Visibility = Visibility.Visible;
+        }
+
+        //zmienne globalne
         Random rand = new Random();
+        DispatcherTimer enemyTimer = new DispatcherTimer();
+        DispatcherTimer targetTimer = new DispatcherTimer();
+        bool humanCaptured = false;
+
+        //dodanie przeciwnika do canvasa
         private void AddEnemy()
         {
             ContentControl enemy = new ContentControl();
@@ -51,6 +91,8 @@ namespace gra1
             canv.Children.Add(enemy);
             
         }
+
+        //animowanie przeciwnika
         private void AnimateEnemy(ContentControl cont, double p1, double p2, string p3)
         {
 
@@ -67,9 +109,24 @@ namespace gra1
             story.Begin();
         }
 
+        //klikniecie przycisku start
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            AddEnemy();
+            //AddEnemy();
+            start();
+        }
+
+        private void start()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            progressBar.Value = 0;
+            startButt.Visibility = Visibility.Collapsed;
+            canv.Children.Clear();
+            canv.Children.Add(human);
+            canv.Children.Add(portal);
+            enemyTimer.Start();
+            targetTimer.Start();
         }
     }
 }
