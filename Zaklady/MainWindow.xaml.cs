ac22449dba031d48644aa93d27c9077af11cb809
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-12 09:42:30 +0200

    dodanie poruszania sie psow

--- Zaklady/MainWindow.xaml.cs
+++ Zaklady/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,11 +22,13 @@ namespace Zaklady
     public partial class MainWindow : Window
     {
         Typo[] typy = {new Typo(50, "Zbychu"), new Typo(50, "Stachu"), new Typo(50, "Mirek")};
+        Random rand = new Random();
+        List<ContentControl> dogs;
         public MainWindow()
         {
             InitializeComponent();
             Load();
-
+            this.dogs = new List<ContentControl> { d1, d2, d3, d4 };
         }
 
         private void Load()
@@ -33,6 +36,46 @@ namespace Zaklady
             RadioButton[] typyRad = { ZbychuRad, StachuRad, MirekRad };
             for(int i = 0;i<3;i++)
             typyRad[i].Content =typy[i].name + " ma " + typy[i].money + "zł";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private double Start(ContentControl dog)
+        {
+            Storyboard story = new Storyboard();
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = 0,
+                To = track.Width-imgDog.Width,
+                Duration = new Duration(TimeSpan.FromSeconds(rand.Next(4, 15)))
+            };
+            story.Children.Add(animation);
+            Storyboard.SetTarget(animation, dog);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(Canvas.Left)"));
+            story.Children.Add(animation);
+            story.Begin();
+            return (double)animation.Duration.TimeSpan.TotalSeconds;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double[] times = {0,0,0,0};
+            for(int i=0;i<4;i++)
+            {
+                repeat:
+                double temp = Start(dogs[i]);
+                for (int j = 0; j < 4; j++)
+                {
+                    if(times[j] == temp)
+                    {
+                        goto repeat;
+                    }
+                }
+                times[i] = temp;
+            }
         }
     }
 }
