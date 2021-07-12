913b9e4d1aee7dcf522b7521d9517b7c0febb7c2
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-13 00:18:51 +0200

    drobne poprawki

--- Zaklady/MainWindow.xaml.cs
+++ Zaklady/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Zaklady
 {
@@ -232,7 +223,6 @@ namespace Zaklady
                     }
                 }
                 times[i] = temp;
-
             }
             // disabling stuffs so people won't break it
             LetTheDogs.IsEnabled = false;
