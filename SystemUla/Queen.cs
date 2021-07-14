2ec7e8ad9a1c1cdd6f16fa6d94da3a824bc1bcae
jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-14 15:15:27 +0200

    Przygotowanie klas pod projekt

---
+++ SystemUla/Queen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemUla
+{
+    class Queen
+    {
+        private Worker[] workers = new Worker[4];
+        private int shiftNumber { get; set; }
+
+        private void AssignWork()
+        {
+
+        }
+
+        private void WorkTheNextShift()
+        {
+
+        }
+    }
+}
---
+++ SystemUla/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemUla
+{
+    class Worker
+    {
+        public string CurrentJob { get; }
+        public int ShiftLefts { get; }
+
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        private void DoThisJob()
+        {
+
+        }
+
+        private void WorkOneShift()
+        {
+
+        }
+    }
+}
