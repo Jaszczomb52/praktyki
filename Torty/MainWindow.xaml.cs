b81b389495e5248ef2cd8929752ad913e38f9deb
jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-14 09:30:15 +0200

    Pierwsza dzialajaca wersja programu do liczenia kosztu zamowienia

--- Torty/MainWindow.xaml.cs
+++ Torty/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace Torty
     /// </summary>
     public partial class MainWindow : Window
     {
+        Party party;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Changed(object sender, TextChangedEventArgs e)
+        {
+            Changing();
+        }
+
+        private void ChangedCheck(object sender, RoutedEventArgs e)
+        {
+            Changing();
+        }
+
+        private void Changing()
+        {
+            try
+            {
+                int numberOfPeople = int.Parse(Text.Text);
+                bool DecorCheck = check2.IsChecked.Value;
+                bool HealthCheck = check1.IsChecked.Value;
+                if (party == null)
+                {
+                    party = new Party(numberOfPeople, DecorCheck, HealthCheck);
+                }
+                else
+                {
+                    party.Refresh(numberOfPeople, DecorCheck, HealthCheck);
+                }
+                Price.Text = party.GetThePrice().ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wprowadź liczbę");
+            }
         }
     }
 }
---
+++ Torty/Party.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torty
+{
+    class Party
+    {
+        private const int foodPerPerson = 25;
+        private int numberOfPeople = 0;
+        private decimal drinksPerPerson = 0;
+        private decimal decorations = 0;
+        private decimal cost = 0;
+        
+
+        public Party(int NumberOfPeople, bool Decorations, bool Health)
+        {
+            Refresh(NumberOfPeople, Decorations, Health);
+        }
+
+        public void Refresh(int NumberOfPeople, bool Decorations, bool Health)
+        {
+            numberOfPeople = NumberOfPeople;
+            CalculateDecorations(Decorations);
+            CalculateCostPerPerson(Health);
+            HowMuch(Health);
+        }
+
+        private void CalculateDecorations(bool fancy)
+        {
+            if(fancy)
+            {
+                decorations = (this.numberOfPeople * 15.00M) + 50M;
+            }
+            else
+            {
+                decorations = (numberOfPeople * 7.50M) + 30M;
+            }
+        }
+
+        private void CalculateCostPerPerson(bool health)
+        {
+            if(health)
+            {
+                drinksPerPerson = 5.00M;
+            }
+            else
+            {
+                drinksPerPerson = 20.00M;
+            }
+        }
+
+        private void HowMuch(bool health)
+        {
+            cost = decorations + ((drinksPerPerson + foodPerPerson) * numberOfPeople);
+            if(health)
+            {
+                cost *= .95M;
+            }
+        }
+
+        public decimal GetThePrice()
+        {
+            return cost;
+        }
+    }
+}
