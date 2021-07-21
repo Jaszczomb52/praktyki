c94fc2a09b9b71919f350be09d5c1c560b77fb98
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-21 08:45:41 +0200

    .

---
+++ IdzNaRyby/MainPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdzNaRyby
+{
+    class MainPlayer: Player 
+    {
+        public MainPlayer(Deck deck, string name):base(deck, name)
+        {
+
+        }
+
+        // tu se beda metody tylko dla glownego gracza (osoba grajaca)
+    }
+}
