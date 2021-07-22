6727b197b7c20dfe0d0282dd659e3dbae8f4dae4
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-22 09:20:42 +0200

    Przeniesienie metod do klasy Game

--- IdzNaRyby/Game.cs
+++ IdzNaRyby/Game.cs
@@ -3,10 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace IdzNaRyby
 {
     class Game
     {
+        readonly Deck deck = new Deck();
+        readonly Random rand = new Random();
+        readonly List<Player> players = new List<Player>();
+
+        public Game()
+        {
+
+        }
+
+        public Player GetPlayer()
+        {
+            return players.Last();
+        }
+
+        public void Checker(Player checker, int selectedIndex, TextBox gameWindow, TextBox groups)
+        {
+            if (selectedIndex >= 0)
+            {
+
+                foreach (Player opponent in players)
+                {
+
+                    if (opponent.Name != checker.Name)
+                    {
+                        int temp = opponent.CheckForCards(checker.DeckOfPlayer.cards[selectedIndex]);
+                        if (temp != 0)
+                        {
+                            gameWindow.Text += opponent.Name + " oddał " + temp + " kart \n";
+                            checker.GiveCards(opponent, selectedIndex, deck);
+                        }
+                    }
+                }
+                CheckGroups(checker, groups);
+                //showOpCards();
+            }
+        }
+
+        public void Start(string name)
+        {
+
+            players.Add(GenerateOpponent("Zbychu"));
+            players.Add(GenerateOpponent("Jacek"));
+            players.Add(GenerateOpponent(name));
+
+            //showOpCards();
+        }
+
+        private void ShowOpCards(TextBox gameWindow)
+        {
+            gameWindow.Text = "";
+            foreach (Card card in players[0].DeckOfPlayer.cards)
+            {
+                gameWindow.Text += card.ToString() + "\n";
+            }
+            foreach (Card card in players[1].DeckOfPlayer.cards)
+            {
+                gameWindow.Text += card.ToString() + "\n";
+            }
+        }
+
+        private void CheckGroups(Player player, TextBox groups)
+        {
+            Card[] group = player.CheckForGroups();
+            if (group[3] != null)
+            {
+                groups.Text += player.Name + " ma grupę " + group[0].Value + " \n";
+            }
+        }
+
+        
+        private Player GenerateOpponent(string name)
+        {
+            Deck deck = StartCards(new Deck(new List<Card>()));
+            Player player;
+            player = new Player(deck, name);
+
+            return player;
+        }
+
+        private Deck StartCards(Deck playersDeck)
+        {
+            for (int i = 0; i < 17; i++)
+            {
+                deck.Shuffle();
+                playersDeck.Add(deck.Deal(rand.Next(deck.Count)));
+            }
+            return playersDeck;
+        }
     }
 }
--- IdzNaRyby/MainWindow.xaml.cs
+++ IdzNaRyby/MainWindow.xaml.cs
@@ -20,96 +20,29 @@ namespace IdzNaRyby
     /// </summary>
     public partial class MainWindow : Window
     {
-        readonly Deck deck = new Deck();
-        readonly Random rand = new Random();
-        readonly List<Player> players = new List<Player>();
+
+        Game game = new Game();
 
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            Start();
-            RefreshHand(hand, players.Last());
-        }
-        
-        private void Start()
-        {
+            game.Start(nameBox.Text);
             gibCard.IsEnabled = true;
             startButton.IsEnabled = false;
             nameBox.IsEnabled = false;
-            players.Add(GenerateOpponent("Zbychu"));
-            players.Add(GenerateOpponent("Jacek"));
-            players.Add(GenerateOpponent(nameBox.Text));
-
-            //showOpCards();
+            RefreshHand(hand, game.GetPlayer());
         }
-
-
-        private Deck StartCards(Deck playersDeck)
-        {
-            for(int i = 0; i<17;i++)
-            {
-                deck.Shuffle();
-                playersDeck.Add(deck.Deal(rand.Next(deck.Count)));
-            }
-            return playersDeck;
-        }
-
-        private Player GenerateOpponent(string name)
-        {
-            Deck deck = StartCards(new Deck(new List<Card>()));
-            Player player;
-            player = new Player(deck, name);
-            
-            return player;
-        }
-
+        
         private void GibCard_Click(object sender, RoutedEventArgs e)
         {
-            Player main = GetPlayer();
-            Checker(main, hand.SelectedIndex) ;
+            Player main = game.GetPlayer();
+            game.Checker(main, hand.SelectedIndex,gameWindow,groups) ;
             RefreshHand(hand, main);
-        }
-
-        private Player GetPlayer()
-        {
-            return players.Last();
-        }
-
-        private void Checker(Player checker, int selectedIndex)
-        {
-            if (selectedIndex >= 0)
-            {
-
-                foreach (Player opponent in players)
-                {
-                    
-                    if (opponent.Name != checker.Name)
-                    {
-                        int temp = opponent.CheckForCards(checker.DeckOfPlayer.cards[selectedIndex]);
-                        if (temp != 0)
-                        {
-                            gameWindow.Text += opponent.Name + " oddał " + temp + " kart \n";
-                            checker.GiveCards(opponent, selectedIndex, deck);
-                        }
-                    }
-                }
-                CheckGroups(checker);
-                //showOpCards();
-            }
-        }
-
-        private void CheckGroups(Player player)
-        {
-            Card[] group = player.CheckForGroups();
-            if(group[3] != null)
-            {
-                groups.Text += player.Name + " ma grupę " + group[0].Value + " \n";
-            }
-
         }
 
         private void RefreshHand(ListBox hand, Player player)
@@ -122,20 +55,6 @@ namespace IdzNaRyby
                 hand.Items.Add(card);
             }
         }
-
-        // metoda debugowa
-
-        private void ShowOpCards()
-        {
-            gameWindow.Text = "";
-            foreach (Card card in players[0].DeckOfPlayer.cards)
-            {
-                gameWindow.Text += card.ToString() + "\n";
-            }
-            foreach (Card card in players[1].DeckOfPlayer.cards)
-            {
-                gameWindow.Text += card.ToString() + "\n";
-            }
-        }
+        
     }
 }
