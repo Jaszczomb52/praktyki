05aed5794d79d6734b9bc7ad86326d529560f97f
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-23 13:42:08 +0200

    dzialajacy prototyp. Nalezy dodac jeszcze kilka zabezpieczen

---
+++ Wymówki/Files.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Wymówki
+{
+    class Files
+    {
+        public string DirectoryPath { get; set; }
+        public DateTime LastFileMod { get; private set; }
+        public DateTime LastFileCreat { get; private set; }
+        public DialogResult GetDirectory()
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                DirectoryPath = dialog.SelectedPath;
+                return res;
+            }
+            else
+            {
+                return res;
+            }
+        }
+
+        public void SaveFile(string text, string result, DateTime time)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.InitialDirectory = DirectoryPath;
+            dialog.Filter = "Pliki tekstowe (*.txt) | *.txt";
+            dialog.DefaultExt = ".txt";
+            dialog.ShowDialog();
+            StreamWriter writer = new StreamWriter(dialog.FileName);
+            writer.WriteLine(text);
+            writer.WriteLine(result);
+            writer.Flush();
+            writer.Close();
+            File.SetLastWriteTime(dialog.FileName, time);
+        }
+
+        public string LoadFile()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = DirectoryPath;
+            dialog.Filter = "Pliki tekstowe (*.txt) | *.txt";
+            dialog.ShowDialog();
+            if (dialog.FileName.Length != 0)
+            {
+                FileStream fs = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fs);
+                LastFileCreat = File.GetCreationTime(dialog.FileName);
+                LastFileMod = File.GetLastWriteTime(dialog.FileName);
+                return reader.ReadLine() + "*;&&;*" + reader.ReadLine();
+            }
+            else
+            {
+                return "no_file";
+            }
+        }
+
+        public string LoadRandomFile()
+        {
+            List<string> files = Directory.GetFiles(DirectoryPath).ToList();
+            Random rand = new Random();
+            string file = files[rand.Next(files.Count)];
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+            LastFileCreat = File.GetCreationTime(file);
+            LastFileMod = File.GetLastWriteTime(file);
+            return reader.ReadLine() + "*;&&;*" + reader.ReadLine();
+        }
+        
+
+    }
+}
---
+++ Wymówki/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Wymówki
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Files dir;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dir = new Files();
+            DialogResult dr = dir.GetDirectory();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                saveButton.IsEnabled = true;
+                loadButton.IsEnabled = true;
+                randomButton.IsEnabled = true;
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (excuseBox.Text != "" && resultBox.Text != "" && dateBox.SelectedDate != null)
+                dir.SaveFile(excuseBox.Text, resultBox.Text, (DateTime)dateBox.SelectedDate);
+            else
+                System.Windows.MessageBox.Show("Wprowadź poprawne dane i wybierz datę ostatniego użycia wymówki");
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] temp = dir.LoadFile().Split(new string[] { "*;&&;*" }, 2, StringSplitOptions.None);
+            excuseBox.Text = temp[0];
+            resultBox.Text = temp[1];
+            fileDateBox.Text = dir.LastFileCreat.ToLongDateString();
+            dateBox.SelectedDate = dir.LastFileMod;
+        }
+
+        private void randomButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] temp = dir.LoadRandomFile().Split(new string[] { "*;&&;*" }, 2, StringSplitOptions.None);
+            excuseBox.Text = temp[0];
+            resultBox.Text = temp[1];
+            fileDateBox.Text = dir.LastFileCreat.ToLongDateString();
+            dateBox.SelectedDate = dir.LastFileMod;
+        }
+    }
+}
