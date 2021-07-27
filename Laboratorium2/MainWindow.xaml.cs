caccadd2639881c7c5fe32ad7ad3ed1bc01096a3
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-27 16:48:56 +0200

    Przygotowanie wszystkich potrzebnych metod

--- Laboratorium2/MainWindow.xaml.cs
+++ Laboratorium2/MainWindow.xaml.cs
@@ -22,37 +22,17 @@ namespace Laboratorium2
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Enemy> enemies;
-        Player player;
-        Point max = new Point();
-        TranslateTransform translate2 = new TranslateTransform
-        {
-            X = 100,
-            Y = 150
-        };
-
+        private Game game;
+        private Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
-            enemies = new List<Enemy> { new Enemy(new Point(100, 150),25) };
-            player = new Player(new Point(0, 100), enemies, inRange,100);
-            max.X = canvas.Width-(rect.Width/2);
-            max.Y = canvas.Height-(rect.Height/2);
-            TranslateTransform translate = new TranslateTransform
-            {
-                X = 0,
-                Y = 100
-            };
             
-            rect.RenderTransform = translate;
-            enemy.RenderTransform = translate2;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            player.Move(e, rect, max);
-            enemies[0].Move(enemy, max, player, translate2);
-            enemies[0].CheckDistance(player.CurrentLoc);
+            
         }
     }
 }
--- Laboratorium2/Weapon.cs
+++ Laboratorium2/Weapon.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows;
 
 namespace Laboratorium2
@@ -7,7 +8,7 @@ namespace Laboratorium2
     {
         public bool PickedUp { get; set; }
 
-        public Weapon(Game game, Point location):base(game, location)
+        public Weapon(Game game, System.Windows.Point location):base(game, location)
         {
             PickedUp = false;
         }
@@ -21,6 +22,7 @@ namespace Laboratorium2
         public abstract void Attack(Direction direction, Random random);
         protected bool DamageEnemy(Direction direction, int radius, int damage, Random random)
         {
+            System.Windows.Point target = game.PlayerLocation;
             for(int distance = 0; distance < radius; distance++)
             {
                 foreach(Enemy enemy in game.Enemies)
@@ -34,6 +36,16 @@ namespace Laboratorium2
                 target = Move(direction, target, game.Boundaries);
             }
             return false;
+        }
+
+        private bool Nearby(System.Windows.Point location, System.Windows.Point target, int distance)
+        {
+            return false;
+        }
+
+        private System.Windows.Point Move(Direction direction, System.Windows.Point target, Rectangle boundaries)
+        {
+            return target;
         }
     }
 }
